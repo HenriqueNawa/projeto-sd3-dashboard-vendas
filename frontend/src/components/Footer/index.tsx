7614437dbc64e4aab414a260746ff946a8f3ed62
Henrique Nawa <henrique.nawa@fatec.sp.gov.br>
2021-05-03 14:37:10 -0300

    Basic static components

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,35 @@
+import ImgDsDark from "assets/img/ds_dark.svg";
+function Footer() {
+  return (
+    <footer className="footer mt-auto py-3 bg-dark">
+      <div className="container">
+        <p className="text-light">
+          App desenvolvido por{" "}
+          <a
+            href="https://br.linkedin.com/in/henriquenawa"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Henrique Nawa
+          </a>
+        </p>
+        <p className="text-light">
+          <small>
+            <strong>Semana Spring React</strong>
+            <br />
+            Evento promovido pela escola DevSuperior:{" "}
+            <a
+              href="https://instagram.com/devsuperior.ig"
+              target="_blank"
+              rel="noreferrer"
+            >
+              @devsuperior.ig
+            </a>
+          </small>
+        </p>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
